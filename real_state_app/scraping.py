b86517f48656ba0e6f27b5e3b1c96996359e5147
yuga-2920 <67497073+yuga-2920@users.noreply.github.com>
2021-04-21 12:33:19 +0900

    Update scraping.py

--- real_state_app/scraping.py
+++ real_state_app/scraping.py
@@ -23,6 +23,25 @@ from logger import logger
 def set_driver(isHeadless=False, isManager=False, isSecret=False):
 
     options = ChromeOptions()
+    
+#      # 起動オプションの設定
+#     options.add_argument('--headless')
+#     options.add_argument('--disable-gpu')
+#     options.add_argument('--no-sandbox')
+#     options.add_argument('log-level=3')
+#     options.add_argument('--ignore-ssl-errors')
+#     options.add_argument('--incognito')          # シークレットモードの設定を付与
+#     options.add_argument('--user-agent=Chrome/87.0.42.88')
+#     options.add_argument('--single-process')
+#     options.add_argument('--start-maximized')
+#     options.add_argument('--ignore-certificate-errors')
+#     options.add_argument('--allow-running-insecure-content')
+#     options.add_argument('--disable-web-security')
+#     options.add_argument('--disable-desktop-notifications')
+#     options.add_argument('--disable-application-cache')
+#     options.add_argument("--disable-extensions")
+#     options.add_argument('--lang=ja')
+
 
     if os.name == 'nt':  # Windows
         driver_path = 'chromedriver.exe'
