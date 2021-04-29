6aa23a1ce382a69f52eab7424ca2718b727e7baf
Yoke <tayok@synspective.com>
2021-04-29 16:58:16 +0900

    instagram

--- instagram_app/app.py
+++ instagram_app/app.py
@@ -155,7 +155,7 @@ class Item():
 
 def main():
 
-    url = 'https://www.mercari.com/jp/search/?sort_order=&keyword=%E3%83%8A%E3%82%A4%E3%82%AD&category_root=2&category_child=&brand_name=&brand_id=&size_group=&price_min=3000&price_max=5000&item_condition_id%5B1%5D=1&status_trading_sold_out=1'
+    url = 'https://www.instagram.com/'
 
     driver = set_driver(isHeadless=False, isManager=True, isExtension=True, profile_path=CHROME_PROFILE_PATH)  # Seleniumドライバ設定
 
@@ -164,50 +164,51 @@ def main():
 
     get_with_wait(driver, url, isWait=True)  # 待機付きページ移動
     soup = parse_html_selenium(driver)
+    logger.debug(soup)
 
-    links = []
-    link_nodes = soup.select('section.items-box a')
+    # links = []
+    # link_nodes = soup.select('section.items-box a')
 
-    for node in link_nodes:
-        links.append(TOP_URL + node.attrs['href'])
+    # for node in link_nodes:
+    #     links.append(TOP_URL + node.attrs['href'])
 
-    items = []
+    # items = []
 
-    start = dt.now().strftime('%Y%m%d_%H%M%S')
+    # start = dt.now().strftime('%Y%m%d_%H%M%S')
 
-    for link in links:
-        logger.debug(f'No.{Item.count + 1}')
-        item = Item(link)
-        items.append(item)
-        item.fetch_info(driver)
-        logger.debug('')
+    # for link in links:
+    #     logger.debug(f'No.{Item.count + 1}')
+    #     item = Item(link)
+    #     items.append(item)
+    #     item.fetch_info(driver)
+    #     logger.debug('')
 
-        break
+    #     break
 
-    end = dt.now().strftime('%Y%m%d_%H%M%S')
-    logger.debug(f'開始時間：{start}, 終了時間：{end}')
+    # end = dt.now().strftime('%Y%m%d_%H%M%S')
+    # logger.debug(f'開始時間：{start}, 終了時間：{end}')
 
-    # ファイル名設定
-    filename = dt.now().strftime('%Y%m%d_%H%M') + '_mercari_demo' + '.xlsx'
+    # # ファイル名設定
+    # filename = dt.now().strftime('%Y%m%d_%H%M') + '_mercari_demo' + '.xlsx'
 
-    keys = items[0].item_info  # 取得情報のキー取得
+    # keys = items[0].item_info  # 取得情報のキー取得
 
-    # 各取得情報の空リスト作成
-    values = []
-    for i in range(len(keys)):
-        values.append([])
-    item_dict = dict(zip(keys, values))
+    # # 各取得情報の空リスト作成
+    # values = []
+    # for i in range(len(keys)):
+    #     values.append([])
+    # item_dict = dict(zip(keys, values))
 
-    # Itemの情報を辞書内のリストに追加
-    for item in items:
-        for k, v in item.item_info.items():
-                item_dict[k].append(v)
+    # # Itemの情報を辞書内のリストに追加
+    # for item in items:
+    #     for k, v in item.item_info.items():
+    #             item_dict[k].append(v)
 
-    df = pd.DataFrame(item_dict)  # ディクショナリをDataFrameに変換
-    df.index += 1  # indexを1始まりに設定
-    excel_save(df, filename)  # Excelファイル保存
-    set_font(filename)  # フォントをメイリオに設定
-    set_border(filename)  # ボーダー追加
+    # df = pd.DataFrame(item_dict)  # ディクショナリをDataFrameに変換
+    # df.index += 1  # indexを1始まりに設定
+    # excel_save(df, filename)  # Excelファイル保存
+    # set_font(filename)  # フォントをメイリオに設定
+    # set_border(filename)  # ボーダー追加
 
     keep_open_driver(driver)
 
