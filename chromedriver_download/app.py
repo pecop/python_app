672cd6a61be344d30f7cac534fb61c632eb8153b
Yoke <tayok@synspective.com>
2021-05-03 16:06:40 +0900

    modified chromedownloader

--- chromedriver_download/app.py
+++ chromedriver_download/app.py
@@ -1,5 +1,10 @@
 from common.driverdownload import get_chrome_driver
 
-version_message = get_chrome_driver()
-print(version_message)
-input('Enterを押すと終了します。')
+def main():
+
+    version_message = get_chrome_driver()
+    print(version_message)
+    input('Enterを押すと終了します。')
+
+if __name__ == '__main__':
+    main()
