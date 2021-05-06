20c8eb4c2952bb355b79f52fe6785765d70a37b2
Yoke <tayok@synspective.com>
2021-05-06 12:43:54 +0900

    path for app of mac

--- mercari_app/dotenv_settings.py
+++ mercari_app/dotenv_settings.py
@@ -5,6 +5,11 @@ from dotenv import load_dotenv
 
 if getattr(sys, 'frozen', False):
     directory_path = os.path.dirname(sys.executable)
+    if '.app' in directory_path:
+        idx = directory_path.find('.app') 
+        directory_path = directory_path[:idx]
+        idx = directory_path.rfind('/')
+        directory_path = directory_path[:idx]
 else:
     directory_path = os.getcwd()
 
