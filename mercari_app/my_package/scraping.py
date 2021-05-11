debf987a043270559abf064d024040fde30b1611
pecop <back.to.the.future52@gmail.com>
2021-05-11 18:32:53 +0900

    modify to get the current directry path for .app correctly

--- mercari_app/my_package/scraping.py
+++ mercari_app/my_package/scraping.py
@@ -27,7 +27,6 @@ def set_driver(isHeadless=False, isManager=False, isSecret=False, isExtension=Fa
 
     options = ChromeOptions()
 
-
     user_agent = [
         'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36',
         'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36',
